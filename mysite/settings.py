f1a88f7967e784f15240158b05b16e09b6f966da
dianis02 <dianis0297@gmail.com>
2023-02-26 16:52:41 -0600

    db connection changed env

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,17 +80,7 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "pythonChat",
-        "USER": "pythonchatuser",
-        "PASSWORD": "Pythonchat5?",
-        "HOST": "localhost",
-        "PORT": "3306",
-    }
-}
-
+DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
