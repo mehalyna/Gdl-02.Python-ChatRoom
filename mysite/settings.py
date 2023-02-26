2f31076363e1fdba48b1611b4b189af3470d7b10
AV1R <avirgariel@gmail.com>
2023-02-26 17:04:10 -0600

    Updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,6 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
 
