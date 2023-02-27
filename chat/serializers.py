085627e8e964dc0be6c7c3e236c0bd14ab3a99af
Juan Pablo García <72479272+juangmalta@users.noreply.github.com>
2023-02-26 19:12:04 -0600

    Update serializers.py

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,8 +1,1 @@
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
 
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = "__all__"
