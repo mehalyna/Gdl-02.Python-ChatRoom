fc78dd3393234ba82d51883a9560afda01d28d04
Roberto Romero Vielma <romero117@outlook.es>
2023-03-06 12:07:32 -0600

    merge chat-user-features into privateChat  branch

--- chat/urls.py
+++ chat/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path("signup/", views.signup, name="signupuser"),
     path("login/", views.signin, name="loginuser"),
     path("create-room/", views.create_room, name="create-room"),
+    path("create-private-chat/", views.create_private_chat, name="create-private-chat"),
     path("allchats/", views.allchats, name="allchats"),
-    path("view-chat/", views.view_chat, name="viewchat"),
+    path("detail/<int:room_id>/", views.view_chat, name="viewchat"),
     # path("view-chat/<int:id_chat>", views.view_chat, name="viewchat"),
     path("logout/", views.logoutuser, name="logoutuser"),
+    path("detail/<int:msg_id>/<int:room_id>/delete", views.deltemsg, name="deletemsg"),
 ]
